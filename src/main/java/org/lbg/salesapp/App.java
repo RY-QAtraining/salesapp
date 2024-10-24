51dfb206c08867f19270eaf73318888827efa269
RY <r.qatraining@gmail.com>
2024-10-24 15:11:56 +0100

    pt2 calculate total cost and allow quit

--- src/main/java/org/lbg/salesapp/App.java
+++ src/main/java/org/lbg/salesapp/App.java
@@ -7,16 +7,32 @@ import java.util.Scanner;
  */
 public class App {
     public static void main(String[] args) {
-        Scanner costInput = new Scanner(System.in);
-        System.out.println("Enter the cost of your item: ");
-        double cost =costInput.nextDouble();
+        while (true) {
+            Scanner vatInput = new Scanner(System.in);
+            System.out.println("Enter the VAT rate of this item: ");
+            double vat = vatInput.nextDouble();
 
-        Scanner vatInput = new Scanner(System.in);
-        System.out.println("Enter the VAT rate of this item: ");
-        double vat = costInput.nextDouble();
+            double total =0;
+            while (true) {
+                Scanner costInput = new Scanner(System.in);
+                System.out.println("Enter the cost of your item: ");
+                double cost = costInput.nextDouble();
+                if (cost == 0)
+                    break;
+                else total += cost;
 
-        double res = finalCost (cost,vat);
-        System.out.println("The final cost of your item is:"+ res);
+            }
+
+            double res = finalCost(total, vat);
+            System.out.println("The final cost of your item is:" + res);
+
+            System.out.println("To perform another calculation, press enter; else type QUIT to exit: ");
+            Scanner userInput = new Scanner(System.in);
+            String user = userInput.nextLine();
+            if ("quit".equalsIgnoreCase(user)){
+                break;
+            }
+        }
     }
 
     public static double finalCost(double c, double v){
